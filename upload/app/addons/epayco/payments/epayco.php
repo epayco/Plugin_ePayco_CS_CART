b9fc90be8038d7e8b11ef484e8b70b4e99ffe35a
Juan Esteban Garcia Valencia <juan.garcia@epayco.com>
2026-03-03 16:20:41 -0500

    Update epayco.php

--- upload/app/addons/epayco/payments/epayco.php
+++ upload/app/addons/epayco/payments/epayco.php
@@ -53,7 +53,7 @@ if ($is_payment_notification) {
         $confirmation = true;
     } else {
         // ePayco redirect - validate against ePayco server
-        $url = 'https://eks-checkout-service.epayco.io/validation/v1/reference/' . $ref_payco;
+        $url = 'https://secure.epayco.co/validation/v1/reference/' . $ref_payco;
         $responseData = @file_get_contents($url);
 
         if ($responseData) {
@@ -368,7 +368,7 @@ if ($is_payment_notification) {
         ]
     )->send();
 
-    // Process the response
+  // Process the response
     if ($response->isRedirect()) {
         // Redirect to ePayco form
         error_log('EPAYCO: Enviando a checkout de ePayco');
